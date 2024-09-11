6e0294377d0fa9d2fb844d03a00828e2454e3d41
Krishna-baheti <u24ai133@aid.svnit.ac.in>
2024-09-11 09:50:07 +0530

    Ques_30.c

--- Ques_30.c
+++ Ques_30.c
@@ -9,34 +9,16 @@
 
 int main()
  {
-  int n, reverse = 0, remainder, original;
-
-  printf("Enter an integer: ");
-  scanf("%d", &n);
-
-  original = n;  
-
-  while (n != 0) 
-  {
-    remainder = n % 10;
-    reverse = reverse * 10 + remainder;
-    n /= 10;
-  }
-
-  if (original % 10 == 0) 
-  {
-    printf("Reversed number = %d", reverse);
-    
-    while (original % 10 == 0)
-     {
-      printf("0");
-      original /= 10;
-    }
-  }
-   else 
-  {
-    printf("Reversed number = %d", reverse);
-  }
-
-  return 0;
-}
+   printf("Enter the number \n");
+   int num, sum = 0, rem;
+   scanf("%d",&num);
+   while(num > 0)
+   {
+    rem = num % 10;
+    sum = sum*10 + rem;
+    num = num/10;
+   }
+   printf("%d",sum);
+   return 0;
+ }
+  
